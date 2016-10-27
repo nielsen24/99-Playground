184fbeb7c3702efbd681be75716247f94cf2dce4
nielsen24 <nielsen24@gmail.com>
2016-10-27 21:44:09 +0200

    now with comment

---
+++ GitTest_GoodbyeWorld_JKN01/GitTest_GoodbyeWorld_JKN01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest_GoodbyeWorld_JKN01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Goodbye World test of Github"); // this is a comment
+
+            Console.ReadKey();
+        }
+    }
+}
